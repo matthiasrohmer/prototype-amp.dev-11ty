007db4f0e6b30dd301e76bdf520e0f7f002fdc14
Matthias Rohmer <rohmer.matthias@anyvent.org>
2020-04-01 16:52:29 +0200

    ✨ Add g.yaml and g.doc constructors to 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,25 @@
-module.exports = {
-  dir: {
-    input: "amp-dev",
-    output: "dist",
-    includes: "_includes",
-    layouts: "_layouts"
+const yaml = require('js-yaml');
+const matter = require('gray-matter');
+
+const {GROW_YAML_SCHEMA} = require('./lib/GrowYamlTypes.js');
+
+const yamlSafeLoad = yaml.safeLoad;
+function loadYaml(input, opts={}) {
+  opts['schema'] = GROW_YAML_SCHEMA;
+  return yamlSafeLoad(input, opts);
+}
+
+matter.engines.yaml.parse = loadYaml;
+yaml.safeLoad = loadYaml;
+
+module.exports = function(eleventyConfig) {
+  return {
+    dir: {
+      input: 'amp-dev',
+      output: 'dist',
+      includes: '_includes',
+      layouts: '_layouts'
+    },
+    markdownTemplateEngine: 'njk'
   }
 };
